0a28108e6dd8dc9838e2d96acaa9a7f7b3dde052
ppreetii <ppreeti.guptaa@gmail.com>
2023-02-23 00:35:47 +0530

    socket.io connection setup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const multer = require("multer");
-const { v4:uuid4} = require("uuid");
+const { v4: uuid4 } = require("uuid");
 
 dotenv.config();
 
@@ -14,31 +14,33 @@ const authRoutes = require("./routes/auth");
 const app = express();
 
 const fileStorage = multer.diskStorage({
-  destination : (req,file,cb) =>{
-    cb(null,'images');
+  destination: (req, file, cb) => {
+    cb(null, "images");
   },
-  filename: (req,file,cb) =>{
-    cb(null, uuid4() + '-' + file.originalname)
-  }
+  filename: (req, file, cb) => {
+    cb(null, uuid4() + "-" + file.originalname);
+  },
 });
 
-const fileFilter = (req,file,cb) =>{
-  if(
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
-  ){
-    cb(null,true)
-  }else{
-    cb(null, false)
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
   }
-}
+};
 
 app.use(bodyParser.json());
-app.use(multer({
-  storage : fileStorage,
-  fileFilter
-}).single('image'))
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter,
+  }).single("image")
+);
 app.use("/images", express.static(path.join(__dirname, "images")));
 //handle CORS issue
 app.use((req, res, next) => {
@@ -53,25 +55,34 @@ app.use((req, res, next) => {
 app.use("/feed", feedRoutes);
 app.use("/auth", authRoutes);
 
-app.use((error, req, res,next)=>{
+app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   let err = {
-    message : error.message
+    message: error.message,
+  };
+
+  if (error.data) {
+    err.data = error.data;
   }
 
-  if(error.data){
-    err.data = error.data
-  }
-
-  res.status(status).json(err)
-})
+  res.status(status).json(err);
+});
 
 mongoose
   .connect(process.env.MONGODB_URL)
   .then((result) => {
     console.log("Database Connected");
-    app.listen(process.env.PORT);
+    const server = app.listen(process.env.PORT);
     console.log(`Server started at port ${process.env.PORT}`);
+
+    const io = require("socket.io")(server,{
+      cors: {
+        origin: '*',
+      }
+    });
+    io.on("connection", (socket) => {
+      console.log("Client Connected");
+    });
   })
   .catch((err) => console.log(err));
